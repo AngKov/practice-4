a48dc1cc564a3a063bf15c46d7a11c79a2b66209
Anzhelika Kovalchuk <AngKov@users.noreply.github.com>
2018-04-09 23:45:42 +0300

    task-1 completed

--- src/task-1.js
+++ src/task-1.js
@@ -2,16 +2,43 @@
 // Change us!
 
 function status(response) {
-    return response;
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    } throw Error(response.statusText);
 }
 
 function json(response) {
-    return response;
+    return response.json();
 }
 
 function getJSON(url) {
-    return null;
+    return window.fetch(url)
+        .then(status)
+        .then(json);
 }
 
 
 export { status, json, getJSON };
+//
+// function status(response) {
+//     if (response.status >= 200 && response.status < 300) {
+//         return Promise.resolve(response);
+//     } else {
+//         return Promise.reject(new Error(response.statusText));
+//     }
+// }
+//
+// function json(response) {
+//     return response.json();
+// }
+//
+// window.fetch('.json')
+//     .then(status)
+//     .then(json)
+//     .then(function(data) {
+//         console.log('Request succeeded with JSON response', data);
+//     }).catch(function(error) {
+//     console.log('Request failed', error);
+// });
+
+
